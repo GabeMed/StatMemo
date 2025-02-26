c1de527d4441f2ac3b6832c6590ce9e8cfcbe1f9
João Pedro Souto Maior Braga <101663774+jpadrono@users.noreply.github.com>
2025-02-26 11:53:47 -0300

    .

--- src/statisticaly_pure_memoizer.py
+++ src/statisticaly_pure_memoizer.py
@@ -1,5 +1,7 @@
 import numpy as np
 import functools
+import statistics
+import math
 
 class StatisticalyPureMemoizer:
     def __init__(self, number_of_executions, max_error=0.5, confidence_level=0.95):
@@ -12,22 +14,61 @@ class StatisticalyPureMemoizer:
         self.revalidated = {}
     
     def _get_number_of_executions(self, f, args):
-        # TODO HERE
-        return 
+        key = (f.__name__, args)
+        if key not in self.cache:
+            return 0
+        return len(self.cache[key]["values"])
     
     def _get_mean_output(self, f, args):
         # TODO HERE
         return 
     
-    def _get_confidence_error(self, f, args):
-        #TODO HERE
-        return
+    def _compute_margin_of_error(self, data):
+
+        n = len(data)
+        if n < 2:
+            return float('inf') 
+        stdev = statistics.pstdev(data)
+        z = 1.96 
+        return z * (stdev / math.sqrt(n))
     
-    def _execute_and_store(self, f, args):
-        #TODO HERE
-        return
+    def _execute_and_store(self, f, key, *args):
+        result = f(*args)
+        self.cache[key]["values"].append(result)
+        return result
     
     def __call__(self, f):
-        #TODOHERE
-        return
+        def wrapper(*args):
+            key = (f.__name__, args)
+            if key not in self.cache:
+                self.cache[key] = {
+                    "values": [],
+                    "mean": None,
+                    "stable": False,
+                    "calls": 0
+                }
+
+            entry = self.cache[key]
+
+            if entry["stable"]:
+                return entry["mean"]
+
+            val = self._execute_and_store(f, key, *args)
+            entry["calls"] += 1
+
+            if entry["calls"] >= self.number_of_executions:
+                data = entry["values"]
+                mean_val = statistics.mean(data)
+                margin = self._compute_margin_of_error(data)
+                entry["mean"] = mean_val
+
+                if margin <= self.max_error:
+                    entry["stable"] = True
+                    return mean_val
+                else:
+                    return val
+            else:
+                return val
+
+        return wrapper
 
